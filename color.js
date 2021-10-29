b06858bec24f510330959351af5f7624369940e3
Willow <93356070+willowthedev@users.noreply.github.com>
2021-10-29 17:21:17 -0500

    Delete color.js

--- color.js
+++
@@ -1,9 +0,0 @@
-function generateRandomColor()
-        {
-            var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-            return randomColor;
-        }
-
-        var color = generateRandomColor();
-        document.body.style.backgroundColor = color
-        document.getElementById("text").textContent=color
